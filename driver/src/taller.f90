a1530115cc4bf20219440b45f8261282ea1c6238
alejandrobodas <alejandroxtr@gmail.com>
2020-05-03 10:20:59 +0100

    gfortran-8.

--- driver/src/taller.f90
+++ driver/src/taller.f90
@@ -5,7 +5,7 @@ PROGRAM TALLER
    integer :: ierr,ncid,dimid,idx,idy
    real :: x(7), y(7)
    character(len = 80)  :: strerror
-   x = 1.1
+   x = 1.0
    y = 2.0
    
    ierr = nf90_create(path="../data/outputs/taller_out.nc", cmode=NF90_WRITE, &

4d637f32f6e2d8014627b473bac16d86b2b9dd14
alejandrobodas <alejandroxtr@gmail.com>
2020-06-19 07:37:30 +0100

    Create test.py

---
+++ driver/test.py
@@ -0,0 +1,12 @@
+import netCDF4,argparse,sys
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import os
+
+#######################
+# Main
+#######################
+if __name__ == '__main__':
+
+        print("Python test.")
